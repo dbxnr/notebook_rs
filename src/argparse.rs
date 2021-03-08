31498c7b3259354417a0e5ea77822fd454b18db8
dbxnr <dbxnr@users.noreply.github.com>
2021-03-08 00:42:52 +0000

    -n takes optional values

--- src/argparse.rs
+++ src/argparse.rs
@@ -12,6 +12,7 @@ pub fn get_args() -> ArgMatches<'static> {
                 .short("n")
                 .long("add")
                 .takes_value(true)
+                .min_values(0)
                 .required(false)
                 .help("Create a new note")
                 .multiple(true),
@@ -24,16 +25,23 @@ pub fn get_args() -> ArgMatches<'static> {
 pub fn parse_args(matches: ArgMatches) -> UserInput {
     let mut cmd = Args::New;
     let mut text = String::new();
-    let mut filename = "_test.txt".to_string();
+    let mut filename = String::new();
     let dt = Local::now();
 
     if matches.is_present("new") {
         cmd = Args::New;
-        text = matches
-            .values_of("new")
-            .unwrap()
-            .collect::<Vec<&str>>()
-            .join(" ");
+
+        text = if matches.index_of("new") == None {
+            "none".to_string()
+        } else {
+            matches
+                .values_of("new")
+                .unwrap()
+                .collect::<Vec<&str>>()
+                .join(" ")
+        }
+        .to_string();
+
         filename = "_test.txt".to_string();
     }
 
