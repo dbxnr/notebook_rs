064128c63476e83aacf01ac12b0be1c6bac3205d
dbxnr <dbxnr@users.noreply.github.com>
2021-04-21 14:52:48 +0100

    add default file path

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 use crate::Notebook;
+use directories::{BaseDirs, UserDirs};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(default)]
@@ -10,13 +11,23 @@ struct NotebookCfg {
 
 impl std::default::Default for NotebookCfg {
     fn default() -> Self {
+        let nb_path = get_documents_dir().join("notebook.md");
         let mut j = HashMap::new();
         let mut nb = Notebook::new();
-        nb.file = "notebook.md".into();
+        nb.file = nb_path.to_str().unwrap().into();
         nb.dt_format = "%A %e %B, %Y - %H:%M".into();
         j.insert("default".to_string(), nb);
         Self { notebooks: j }
     }
+}
+
+pub fn get_documents_dir() -> PathBuf {
+    // If XDG_DOCUMENT_DIR does not exist, returns XDG_HOME
+    let base_dirs = BaseDirs::new().unwrap();
+    let home_dir = base_dirs.home_dir();
+    let user_dirs = UserDirs::new().unwrap();
+    let document_dir = user_dirs.document_dir().unwrap_or(home_dir);
+    PathBuf::from(document_dir)
 }
 
 pub fn read_config(notebook: Option<&str>) -> Result<Notebook, confy::ConfyError> {
